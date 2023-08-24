687b72891c8ba6b08a83195c533755a2c2eb2a3c
amusukwa <amusukwa@gmail.com>
2023-08-24 10:52:07 -0700

    add

---
+++ add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "monty.h"
+#include <stdlib.h>
+
+/**
+ * add - adds top two value in the stack
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the operation
+ */
+
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->n + (*stack)->next->n;
+	pop_from_stack(stack, line_number);
+	(*stack)->n = sum;
+}
